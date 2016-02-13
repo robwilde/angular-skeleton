1e017f9a93fc5fc4f5e8dda0f52cb6cd92fc20db
Joe Greubel <jgreub@gmail.com>
2016-02-12 19:41:57 -0800

    Update dependencies and remove unused import in karma.conf

--- karma.conf.js
+++ karma.conf.js
@@ -3,7 +3,6 @@ module.exports = function(config) {
 
     files: [
       'node_modules/angular/angular.js',
-      'node_modules/angular-route/angular-route.js',
       'node_modules/angular-mocks/angular-mocks.js',
       'app/**/*.module.js',
       'app/**/*.js',
